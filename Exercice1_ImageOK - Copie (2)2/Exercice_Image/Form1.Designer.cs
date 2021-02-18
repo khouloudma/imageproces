026b5644e51efd7de9f062d25824d947831f1cfe
khouloudma <khouloudmabrouk00@gmail.com>
2021-02-18 21:01:33 +0100

    testing

---
+++ Exercice1_ImageOK - Copie (2)2/Exercice_Image/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+namespace Exercice_Image
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "load image\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(142, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(418, 184);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(578, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(418, 195);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 107);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(113, 212);
+            this.listBox1.TabIndex = 4;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(142, 213);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(418, 372);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(578, 213);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(418, 372);
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "frame";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 597);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button2;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+    }
+}
+
---
+++ Exercice1_ImageOK - Copie (2)2/Exercice_Image/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV.UI;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+//using DirectShowLib;
+using Emgu.CV.Features2D;
+using System.Diagnostics;
+
+
+
+namespace Exercice_Image
+{
+    public partial class Form1 : Form
+    {
+        // Create new stopwatch.
+        Stopwatch stopwatch = new Stopwatch();
+
+          public Form1()
+        {
+            InitializeComponent();
+            listBox1.Items.Clear();
+           pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+        Image<Bgr, Byte> ImageFrame;          //    
+                                              Image<Bgr, Single> ImageColor;      //
+
+        Capture capture;        //  takes images from camera as image frames
+        private bool captureInProgress; //  checks if capture is executing
+
+        //Image<Bgr, byte> img;
+        //Image<Bgr, float> fimg;
+        Image<Gray, Byte> ImageGray;      //il faut ajouter "opencv_imgproc220.dll"
+        Image<Lab, Single> ImageLab;
+        
+
+        //public object ImageHsv { get; private set; }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // OK : ouvrir image depuis disque
+                string FNom = openFileDialog1.FileName;
+                listBox1.Items.Add(FNom.ToString());
+
+                Image<Bgr, byte> img = new Image<Bgr, byte>(new Bitmap(@FNom));
+                pictureBox1.Image = img.ToBitmap();
+
+
+                img = img.Rotate(25, new Bgr(10, 10, 100));
+                pictureBox2.Image = img.ToBitmap();
+
+            }
+        }
+        //==========================================================================
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            ImageFrame = capture.QueryFrame();
+
+            ImageColor = ImageFrame.Convert<Bgr, Single>() / 255; 	
+
+		//SepareMask(ImageColor, trackBar1.Value, trackBar2.Value);
+            	//textBox1.Text = " TBar(" + trackBar1.Value + "%, " + trackBar2.Value + "%)";
+
+            //ImageHsv = ImageFrame.Convert<Hsv, byte>();   		
+            
+            ImageLab = ImageFrame.Convert<Lab, Single>() / 255;     	
+            
+            ImageGray = ImageFrame.Convert<Gray, byte>();
+
+            //ImageGray = GetRMask(ImageFrame, trackBar1.Value, trackBar2.Value);
+            // ImageFrame = ImageGray.Convert<Bgr, byte>();
+            //           Image<Gray, Byte> smallGrayFrame = ImageGray.PyrDown();
+            //            Image<Gray, Byte> smoothedGrayFrame = smallGrayFrame.PyrUp();
+            //            Image<Gay, Byte> cannyFrame = smoothedGrayFrame.Canny(new Gray(100), new Gray(60));
+            //            pictureBox2.Image = cannyFrame.ToBitmap();
+            //ImageColor = ImageFrame.Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR, true);
+            pictureBox2.Image = ImageGray.ToBitmap();
+
+            pictureBox4.Image = ImageFrame.ToBitmap();
+            pictureBox3.Image = ImageLab.ToBitmap();
+
+            // ImageColor.Canny(new Bgr(100, 100, 100), new Bgr(60, 60, 60));
+
+            ImageGray = ImageFrame.Convert<Gray, byte>();
+
+            Image<Gray, Byte> cannyFrame = ImageGray.Canny(new Gray(100), new Gray(60));
+            pictureBox1.Image = cannyFrame.ToBitmap();
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            #region if capture is not created, create it now
+            if (capture == null)
+            {
+                try
+                {
+                    
+                    capture = new Emgu.CV.Capture(0);
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+            #endregion
+
+            if (capture != null)
+            {
+                if (captureInProgress)
+                {  //if camera is getting frames then stop the capture and set button Text
+                   // "Start" for resuming capture
+
+                    button2.Text = "Start!"; //
+                    Application.Idle -= ProcessFrame;
+                }
+                else
+                {
+                    //if camera is NOT getting frames then start the capture and set button
+                    // Text to "Stop" for pausing capture
+
+                    button2.Text = "Stop";
+                    Application.Idle += ProcessFrame;
+                }
+                captureInProgress = !captureInProgress;
+            }
+        }
+        //======================================================================
+        private void ReleaseData()
+        {
+            if (capture != null)
+                capture.Dispose();
+        }
+
+       
+    } //class fin
+} // namespace
+
